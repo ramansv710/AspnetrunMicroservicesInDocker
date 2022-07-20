0dd6ff0a68ebf504077be35a9eafd380d0fe9241
Raman Srinivasa Venkat <ramansv710@gmail.com>
2022-07-20 22:27:03 +0530

    discount api docker changes.

--- src/Services/Discount/Discount.API/Controllers/DiscountController.cs
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -17,13 +17,13 @@ namespace Discount.API.Controllers
         {
             _repository = repository ?? throw new ArgumentNullException(nameof(repository));
         }
-
+        
         [HttpGet("{productName}", Name = "GetDiscount")]
         [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<Coupon>> GetDiscount(string productName)
         {
-            var coupon = await _repository.GetDiscount(productName);
-            return Ok(coupon);
+            var discount = await _repository.GetDiscount(productName);
+            return Ok(discount);
         }
 
         [HttpPost]
@@ -36,7 +36,7 @@ namespace Discount.API.Controllers
 
         [HttpPut]
         [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
-        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        public async Task<ActionResult<Coupon>> UpdateBasket([FromBody] Coupon coupon)
         {
             return Ok(await _repository.UpdateDiscount(coupon));
         }
--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -18,6 +18,6 @@ namespace Discount.API
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                });       
     }
 }
