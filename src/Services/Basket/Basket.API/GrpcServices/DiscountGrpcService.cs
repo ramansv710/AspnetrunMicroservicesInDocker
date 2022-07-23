df0f23a032e7c11008e9567f1c4b22f7345e63f1
Raman Srinivasa Venkat <ramansv710@gmail.com>
2022-07-23 14:18:34 +0530

    Ordering Api push.

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,22 @@
+﻿using Discount.Grpc.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountGrpcService;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountGrpcService)
+        {
+            _discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+            return await _discountGrpcService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
